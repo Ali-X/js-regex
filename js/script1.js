b38a5ebeab22d6c2c7cf6bbf6443400168b3c0c0
Ali-X <alina240895@yandex.ru>
2019-02-11 19:41:59 +0200

    Implement first task

--- js/script1.js
+++ js/script1.js
@@ -1,10 +1,19 @@
 "use strict";
 
+console.log(findTime(prompt('Enter date:', 'Завтрак в 19:00')));
 
 function findTime(str) {
-  let time;
+  let regex = new RegExp("\\b(\\d{1,2}):(\\d{2})\\b");
+  let time = str.match(regex);
 
-  return time;
+  if (time) {
+    let hours = time[1];
+    let minutes = time[2];
+
+    if (hours >= 0 && hours <= 24 && minutes >= 0 && minutes < 60) {
+      return time[0];
+    }
+  }
+
+  return 'No correct time in string'
 }
-
-console.log(findTime('Breakfast in 32:60'));
