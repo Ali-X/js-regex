26a1134b986668074fccf947c3ce709b87011614
Ali-X <alina240895@yandex.ru>
2019-02-11 22:33:41 +0200

    Implement second task

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,25 @@
 "use strict";
 
+function findExpression(str) {
+  let regex = /^(-|\d)+\s?[+-/]\s?(-|\d)+$/g;
+  let exp = str.match(regex);
+  console.log(`Input text: ${str}`);
+
+  if (exp) {
+    regex = /(-|\d)+|[+-/]|(-|\d)+/g;
+    exp = str.match(regex);
+    console.log(`First operand: ${exp[0]}`);
+    console.log(`Operator: ${exp[1]}`);
+    console.log(`Second operand: ${exp[2]}`);
+  } else {
+    console.log('There is no expression');
+  }
+
+  console.log('\n');
+}
+
+findExpression('1 + 2');
+findExpression('1+2');
+findExpression('1.2 3.4');
+findExpression('-3 / -6');
+findExpression('-2 - 2');
