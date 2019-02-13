30fb89a12a37f2d6e5e60e38d04f2e90eeeb1bb2
Alina Neshchebnaya <alina.neshchebna@scalors.com>
2019-02-13 14:24:27 +0200

    Modify regex

--- js/script1.js
+++ js/script1.js
@@ -3,16 +3,11 @@
 console.log(findTime(prompt('Enter date:', 'Завтрак в 19:00')));
 
 function findTime(str) {
-  let regex = new RegExp("\\b(\\d{1,2}):(\\d{2})\\b");
+  let regex = new RegExp("\\b(0\\d|1\\d|2[0-3]):(0\\d|[1-5]\\d)\\b");
   let time = str.match(regex);
 
   if (time) {
-    let hours = time[1];
-    let minutes = time[2];
-
-    if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
-      return time[0];
-    }
+    return time[0];
   }
 
   return 'No correct time in string'
