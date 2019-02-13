a3d4226f240cbccc7260a542f7c688e3542e3134
Alina Neshchebnaya <alina.neshchebna@scalors.com>
2019-02-13 14:25:45 +0200

    Modify regex

--- js/script1.js
+++ js/script1.js
@@ -3,7 +3,7 @@
 console.log(findTime(prompt('Enter date:', 'Завтрак в 19:00')));
 
 function findTime(str) {
-  let regex = new RegExp("\\b(0\\d|1\\d|2[0-3]):(0\\d|[1-5]\\d)\\b");
+  let regex = new RegExp("\\b([0-1]\\d|2[0-3]):(0\\d|[1-5]\\d)\\b");
   let time = str.match(regex);
 
   if (time) {
