131ad291492d64cb5ea0191dc5ed87b1265aea68
Alina Neshchebnaya <alina.neshchebna@scalors.com>
2019-02-13 13:43:11 +0200

    Modify regex

--- js/script2.js
+++ js/script2.js
@@ -1,16 +1,14 @@
 "use strict";
 
 function findExpression(str) {
-  let regex = /^(-|\d)+\s?[+-/]\s?(-|\d)+$/g;
-  let exp = str.match(regex);
+  let regex = /(^\s*-?\d+\.?\d*)\s*([+-\/*])\s*(-?\d+\.?\d*$)/g;
+  let exp = regex.exec(str);
   console.log(`Input text: ${str}`);
 
   if (exp) {
-    regex = /(-|\d)+|[+-/]|(-|\d)+/g;
-    exp = str.match(regex);
-    console.log(`First operand: ${exp[0]}`);
-    console.log(`Operator: ${exp[1]}`);
-    console.log(`Second operand: ${exp[2]}`);
+    console.log(`First operand: ${exp[1]}`);
+    console.log(`Operator: ${exp[2]}`);
+    console.log(`Second operand: ${exp[3]}`);
   } else {
     console.log('There is no expression');
   }
@@ -21,5 +19,10 @@ function findExpression(str) {
 findExpression('1 + 2');
 findExpression('1+2');
 findExpression('1.2 3.4');
+findExpression('1.2 + 3.4');
 findExpression('-3 / -6');
 findExpression('-2 - 2');
+findExpression('.6   *   2');
+findExpression('1.5 + 2');
+findExpression('  5   +   2');
+findExpression('5- -2');
